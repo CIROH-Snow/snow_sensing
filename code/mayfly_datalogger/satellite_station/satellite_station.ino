0885e5a05151c5d816c58d7fe6041bac6cfbe94d
Braedon Dority <braedon.dority@usu.edu>
2024-12-30 13:10:49 -0700

    Update satellite_station.ino

--- code/mayfly_datalogger/satellite_station/satellite_station.ino
+++ code/mayfly_datalogger/satellite_station/satellite_station.ino
@@ -76,7 +76,7 @@ const char* sketchName = "satellite_station.ino";
 // This can be found on the bottom of the Mayfly if the ink hasn't rubbed off.
 // Sometimes I just use the name I've given the site here instead of the serial number.
 // Make sure it is a string (in double quotations).
-const char* LoggerID = "sunny";
+const char* LoggerID = "marshes";
 
 // How frequently (in minutes) to log data
 const uint8_t loggingInterval = 60;
@@ -155,8 +155,8 @@ AltSoftSerial sonarSerial(6, -1);  // The -1 indicates that no Tx wire is attach
 								   // on the board, pin 6).
 
 // Set the height of the sensor (in millimeters)
-const int32_t sonarHeight = 1968.5;
-const double slopeAngleDeg = 24;
+const int32_t sonarHeight = 2247.9;
+const double slopeAngleDeg = 0;
 double slopeAngleRad = slopeAngleDeg * 3.14159 / 180;
 
 // There is no need to continuously run this sensor when it isn't taking a measurement,
@@ -258,7 +258,7 @@ the address of the ADC you need to connect to:
   
 */
 
-float sp510calibFactor = 22.93;
+float sp510calibFactor = 26.03;
 
 // Construct the Apogee SP-510-SS sensor object
 ApogeeSP510 sp510(-1, sp510calibFactor, 0x4B, 3);  // The -1 indicates that there is no powering up necessary for measurement 
@@ -290,7 +290,7 @@ Variable* sp510rad =
   CLEAR   -> V12- (BATTERY GROUND)
 */
 
-float sp610calibFactor = 32.05;
+float sp610calibFactor = 30.82;
 
 // Construct the Apogee SP-610-SS sensor object
 ApogeeSP610 sp610(-1, sp610calibFactor, 0x4B, 3);
@@ -319,8 +319,8 @@ Variable* sp610rad =
   CLEAR   -> V12- (BATTERY GROUND)
 */
 
-float sl510k1 = 9.007;
-float sl510k2 = 1.026;
+float sl510k1 = 9.509;
+float sl510k2 = 1.020;
 
 // Construct the Apogee SL-510-SS sensor object
 // The only parameter you should adjust is how many measurements you want to take (last parameter)
@@ -354,8 +354,8 @@ Variable* sl510rad =
   CLEAR   -> V12- (BATTERY GROUND)
 */
 
-float sl610k1 = 9.021;
-float sl610k2 = 1.031;
+float sl610k1 = 9.323;
+float sl610k2 = 1.033;
 
 // Construct the Apogee SL-610-SS sensor object
 // The only parameter you should adjust is how many measurements you want to take (last parameter)
@@ -871,7 +871,7 @@ void loop() {
   (45 min * 60 sec/min). If you want them to turn on at the 15-minute mark, for example,
   set it equal to 15 min * 60 sec/min = 900
   */
-  if (dataLogger.getNowLocalEpoch() % (loggingInterval * 60) == 1200) {  // Check if we are on the 45th minute (2700 seconds into the interval)
+  if (dataLogger.getNowLocalEpoch() % (loggingInterval * 60) == 2400) {  // Check if we are on the 45th minute (2700 seconds into the interval)
     digitalWrite(22, HIGH);  // Power up the power relay (pin 22 is the Mayfly's switched power output)
     delay(1000);
     digitalWrite(powerRelayPin, HIGH);  // Set the signal pin high (to close the circuit, the relay is waiting for a signal drop)
